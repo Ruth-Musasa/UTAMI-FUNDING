bae974ec6928ab2b1d672310debef67a9ba5a978
Ruth-Musasa <ruthebondo2001@gmail.com>
2024-04-06 16:22:48 +0200

    change

---
+++ server/db.js
@@ -0,0 +1,13 @@
+const knex = require('knex');
+const db = knex({
+  client: 'pg',
+  connection: {
+    connectionString: 'the database URL',
+    ssl: {
+      rejectUnauthorized: false
+    }
+  }
+});
+
+module.exports = db;
+
