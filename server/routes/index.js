44404f79c84808603bfbc3db0cafc46393893117
Ruth-Musasa <ruthebondo2001@gmail.com>
2024-04-09 10:31:13 +0200

    changement du msg affichage dans le index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: "Page d'accueil" });
+  res.render('index', { title: "Page d'accueil de API UTAMI" });
 });
 
 module.exports = router;
