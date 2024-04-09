1fef63fd1835d592da3e834d5afd4c42b1bb1cf6
Ruth-Musasa <ruthebondo2001@gmail.com>
2024-04-09 10:51:21 +0200

    essaie chelou de deployement

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
-
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: "Page d'accueil de API UTAMI" });
 });
+
+router.get('/use', function (req, res, next) {
+  res.render('index', { title: "Page users de API UTAMI" });
+});
+
 
 module.exports = router;
