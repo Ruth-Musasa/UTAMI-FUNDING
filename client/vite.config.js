967e2cdd6ddf95ca4ff1a9e823b2f58c140166de
Ruth-Musasa <ruthebondo2001@gmail.com>
2024-04-13 11:32:42 +0200

    arrangement de URL dans vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://Ruth-Musasa.github.io/UTAMI-FUNDING/",
   optimizeDeps: {
     include: ['react-router-dom'],
   },
