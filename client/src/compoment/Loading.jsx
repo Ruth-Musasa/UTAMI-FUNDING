5366f80d89ef232df21fc34ab81c2e821d62e6cb
Ruth-Musasa <ruthebondo2001@gmail.com>
2024-05-10 12:41:23 +0200

    update du loader

--- client/src/compoment/Loading.jsx
+++ client/src/compoment/Loading.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Loading = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="border-8 border-gray-200 border-t-8 border-b-8 rounded-full w-24 h-24 animate-spin"></div>
+    <div class="flex justify-center items-center mt-24 h-96">
+      <div class="w-16 h-16 border-t-4 border-cyan-700 rounded-full animate-spin"></div>
     </div>
   );
 };
